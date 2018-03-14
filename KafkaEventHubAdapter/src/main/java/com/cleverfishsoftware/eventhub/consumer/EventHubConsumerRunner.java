614b5ded1e3c09dd181aa1ff6af661e0f019d8e3
petergdoyle <peter.doyle@travelport.com>
2018-03-14 15:38:59 -0600

    it works - but had to create a Consumer Group in Azure EventHub

--- KafkaEventHubAdapter/src/main/java/com/cleverfishsoftware/eventhub/consumer/EventHubConsumerRunner.java
+++ KafkaEventHubAdapter/src/main/java/com/cleverfishsoftware/eventhub/consumer/EventHubConsumerRunner.java
@@ -2,12 +2,9 @@
  */
 package com.cleverfishsoftware.eventhub.consumer;
 
-import static com.cleverfishsoftware.kafka.adapters.eventhub.KafkaEventHubAdapterUtils.CreateConnectionString;
 import static com.cleverfishsoftware.kafka.adapters.eventhub.KafkaEventHubAdapterUtils.CreateEventProcessorHost;
 import java.io.IOException;
-import java.util.Properties;
 import static com.cleverfishsoftware.kafka.adapters.eventhub.KafkaEventHubAdapterUtils.LoadEventHubProperties;
-import com.microsoft.azure.eventhubs.ConnectionStringBuilder;
 import com.microsoft.azure.eventprocessorhost.EventProcessorHost;
 import com.microsoft.azure.eventprocessorhost.EventProcessorOptions;
 import java.util.concurrent.ExecutionException;
@@ -19,23 +16,12 @@ public class EventHubConsumerRunner {
 
     public static void main(String[] args) throws IOException {
 
-        final Properties ehProperties = LoadEventHubProperties();
-
-        EventHubConsumer ehConsumer = new EventHubConsumer(ehProperties);
-
-//        final String consumerGroupName = ehProperties.getProperty("ConsumerGroupName"); //"$Default";
-//        final String serviceBusNamespaceName = ehProperties.getProperty("ServiceBusNamespaceName"); //"----ServiceBusNamespaceName-----";
-//        final String eventHubName = ehProperties.getProperty("EventHubName"); //"----EventHubName-----";
-//        final String sasKeyName = ehProperties.getProperty("SharedAccessKeyName");//-----SharedAccessSignatureKeyName-----";
-//        final String sasKey = ehProperties.getProperty("SharedAccessKey"); //"---SharedAccessSignatureKey----";
-//
-//        final String storageConnectionString = ehProperties.getProperty("StorageConnectionString");
-
-        EventProcessorHost host = CreateEventProcessorHost(ehProperties);
-
+        EventProcessorHost host = CreateEventProcessorHost(LoadEventHubProperties());
         System.out.println("Registering host named " + host.getHostName());
+        
         EventProcessorOptions options = new EventProcessorOptions();
         options.setExceptionNotification(new ErrorNotificationHandler());
+        
         try {
             host.registerEventProcessor(EventProcessor.class, options).get();
         } catch (Exception e) {
@@ -54,7 +40,7 @@ public class EventHubConsumerRunner {
             host.unregisterEventProcessor();
 
             System.out.println("Calling forceExecutorShutdown");
-//            host..forceExecutorShutdown(120);
+//            host.forceExecutorShutdown(120);
 
         } catch (Exception e) {
             System.out.println(e.toString());
